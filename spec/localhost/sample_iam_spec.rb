35a369ffd6fca6b55e70790b9abdb4b431a3ac71
Alex Lubneuski <alex.lubneuski@gmail.com>
2016-06-13 08:40:38 -0400

    added another test for inline policy document

--- spec/localhost/sample_iam_spec.rb
+++ spec/localhost/sample_iam_spec.rb
@@ -5,9 +5,9 @@ describe 'the example network' do
   before :each do
    @iam = iam_role('ApplicationCF-ApplicationCFRole-ZWAW9ALDHVSI')
   end
-
+=begin
     it "iam has policy" do
-      @iam.should be_has_attached_policy('')
+      @iam.should be_has_attached_policy(nil)
     end
 
     it "iam has number of attached policies" do
@@ -16,6 +16,14 @@ describe 'the example network' do
 	
     it "iam has number of inline policies" do
       @iam.should be_has_number_of_inline_policies(6)
+    end
+=end
+    it "iam has attached policy document" do
+      @iam.should be_has_assume_role_policy_document({"Version"=>"2012-10-17", "Statement"=>[{"Effect"=>"Allow", "Principal"=>{"AWS"=>["arn:aws:iam::241449424531:root", "arn:aws:iam::654962015416:root"], "Service"=>"servicecatalog.amazonaws.com"}, "Action"=>"sts:AssumeRole"}]})
+    end
+
+    it "iam has inline policy " do
+      @iam.should be_has_inline_policy("ApplicationCloudFormationEC2Policy")
     end
 
 end
